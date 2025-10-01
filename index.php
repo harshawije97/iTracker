327d8273704d8282aa9fe47746ecf0eec9482270
harshawije97 <harshawije99@gmail.com>
2025-10-01 15:15:51 +0530

    connection update

--- index.php
+++ index.php
@@ -1,31 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>iTracker - Sign In</title>
-  <link rel="stylesheet" href="./public/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>iTracker - Sign In</title>
+    <link rel="stylesheet" href="./public/style.css">
 </head>
+
 <body>
-  <div class="login-container">
-    <div class="app-name">iTracker</div>
-    <h1 class="heading">Sign In</h1>
+    <?php include_once './database/connection.php'; ?>
     
-    <form>
-      <div class="form-group">
-        <input type="text" class="input-field" placeholder="Username" required>
-      </div>
-      
-      <div class="form-group">
-        <input type="password" class="input-field" placeholder="Password" required>
-      </div>
-      
-      <button type="submit" class="continue-btn">Continue</button>
-    </form>
-    
-    <div class="footer-text">
-      Don't have an account? <a href="#">Request Access</a>
+    <div class="login-container">
+        <div class="app-name">iTracker</div>
+        <h1 class="heading">Sign In</h1>
+
+        <form>
+            <div class="form-group">
+                <input type="text" class="input-field" placeholder="Username" required>
+            </div>
+
+            <div class="form-group">
+                <input type="password" class="input-field" placeholder="Password" required>
+            </div>
+
+            <button type="submit" class="continue-btn">Continue</button>
+        </form>
+
+        <div class="footer-text">
+            Don't have an account? <a href="#">Request Access</a>
+        </div>
     </div>
-  </div>
 </body>
+
 </html>
